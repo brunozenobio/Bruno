f530db6e5496c30e6f0c84b3ca0d41069a50b068
brunozenobio4 <brunozenobio4@gmail.com>
2022-12-05 13:05:15 -0300

    2dasubida

--- Pendulosimplefortran.f90
+++ Pendulosimplefortran.f90
@@ -4,7 +4,7 @@ program name
     !Condiciones iniciales
     real(wp)             ::theta0,omega0,t0=0.0_wp,tf=100.0_wp
     !Constantes
-    real(wp),parameter   :: g=-9.8_wp,l=1.0_wp
+    real(wp),parameter   :: g=-9.8_wp,l=1.0_wp,gamma=0.5_wp
     !Variables 
     real(wp)             :: t,omega,theta,paso
     !Coordenadas cartesianas
@@ -20,18 +20,18 @@ program name
     open(10,file='datospen.dat')
     do i=0,nupasos
         t=t0+i*paso
-        omega=omega0+paso*f(theta0,g,l)
+        omega=omega0+paso*f(omega0,theta0,g,l,gamma)
         theta=theta0+paso*omega0
         x=l*sin(theta)
         y=l*cos(theta)
-        write(10,*) t,x,y
+        write(10,*) t,',',x,',',y
         omega0=omega
         theta0=theta
     enddo
 contains
-    real(wp) function f(y,g,l)
-        real(wp), intent(in) ::y
-        real(wp) :: g,l
-        f=-g/l*sin(y)
+    real(wp) function f(x,y,g,l,gamma)
+        real(wp), intent(in) ::y,x
+        real(wp) :: g,l,gamma
+        f=-g/l*sin(y)-gamma*l*x
     end function f
 end program name
