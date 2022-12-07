1e82b291595954e5512c3506a021c03f91418421
brunozenobio4 <brunozenobio4@gmail.com>
2022-12-07 20:08:11 -0300

    nuevo

--- pendvert.f90
+++ pendvert.f90
@@ -4,9 +4,9 @@ program name
     !Condiciones iniciales
     real(wp)             ::y0,dy0,t0=0.0_wp,tf=100.0_wp
     !Constantes
-    real(wp),parameter   :: g=-9.8_wp,l=3.0_wp,gamma=0.0_wp
+    real(wp),parameter   :: g=-9.8_wp,l=1.0_wp,gamma=0.5_wp
     !Variables 
-    real(wp)             :: t,dy,paso,y
+    real(wp)             :: t,dy,paso,y,y1,dy1
     !Coordenadas cartesianas
     real(wp)             :: xx,yy,h,k1,k2,k3,k4
     real(wp)             :: l1,l2,l3
@@ -20,26 +20,20 @@ program name
     paso=(tf-t0)/nupasos
     h=paso
     open(10,file='datosprk.dat')
+    y1=y0-dy0*h+h*h*f(y0,dy0)/2
     do i=0,nupasos
+        y1=y0
+        y0=y0+h*dy0+h**2*f(y0,dy0)/2
+        dy0=dy0+h*(f(y1,dy1)+f(y0,dy0))/2
         t=t0+i*paso
-        k1=paso*dy0
-        l1=paso*f(y0,dy0)
-        k2=paso*(dy0+l1/2)
-        l2=paso*f(y0+k1/2,dy0+l1/2)
-        k3=paso*(dy0-l1+2*l2)
-        l3=paso*f(y0-k1+2*k2,dy0-l1+2*l2)
-        y=y0+(k1+4*k2+k3)/6
-        dy=dy0+(l1+4*l2+l3)/6
-        xx=l*sin(y)
-        yy=-l*cos(y)
+        xx=l*sin(y0)
+        yy=-l*cos(y0)
         write(10,*)t,',',xx,',',yy
-        dy0=dy
-        y0=y
     enddo
 contains
     real(wp) function f(y,x)
         real(wp), intent(in) ::y,x
-        real(wp),parameter :: g=9.81_wp,l=3.0_wp,gamma=0.1_wp
+        real(wp),parameter :: g=9.81_wp,l=1.0_wp,gamma=1.0_wp
         f=-g/l*sin(y)-gamma*l*x
     end function f
 end program name
